2684673c01bf3626800ea5c0a39c9cdbcf4a59fa
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-07 13:43:14 +0100

    Refactor fetching user data

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import AdviceSection from './sections/Advice'
 import ArtsSection from './sections/Art'
 import UsersSection from './sections/Users'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const fetchData = (query, action) => {
+    console.log('hello')
+      fetch(`https://boolean-uk-api-server.fly.dev${query}`)
+        .then(response => response.json())
+        .then(data => action(data));
+        // .then(data => console.log(data));
+    }
+
+  const [usersData, setUsersData] = useState(null);
+
+  useEffect((() => fetchData('/pudkipz/contact'), setUsersData), [])
+
   return (
     <div className="main-layout">
       <ArtsSection />
-      <UsersSection />
+      {usersData && <UsersSection usersData={usersData} />}
       <AdviceSection />
     </div>
   )
--- src/sections/Users/components/UsersList.jsx
+++ src/sections/Users/components/UsersList.jsx
@@ -1,18 +1,6 @@
-import {useEffect, useState} from 'react';
 import UsersListItem from './UsersListItem';
 
-export default function UsersList() {
-  const fetchAllUsers = () => {
-    fetch(`https://boolean-uk-api-server.fly.dev/pudkipz/contact`)
-      .then(response => response.json())
-      .then(data => setUsersData(data));
-      // .then(data => console.log(data));
-  }
-
-  const [usersData, setUsersData] = useState(null);
-
-  useEffect(fetchAllUsers, [])
-  
+export default function UsersList(usersData) {
   return (
     <ul className='users-list'>
       {usersData && usersData.map((usersItem, i) =>
